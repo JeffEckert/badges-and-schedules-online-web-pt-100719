85a21811cf188bf2621a2cbe5a7e4296c95f334b
Jeffery Eckert <bromodo@gmail.com>
2019-10-14 00:45:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
 def batch_badge_creator(array)
   new_array = []
-  array.each {array << new_array puts "Hello, my name is #{array}."}
+  array.each {|name| array << new_array puts "Hello, my name is #{name}."}
 end
