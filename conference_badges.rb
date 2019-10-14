cb750e3ddcd0e3099c8fb6b9a3d95cf2f2720414
Jeffery Eckert <bromodo@gmail.com>
2019-10-14 01:03:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
  "hello, my name is #{name}."
 end
   
-  def batch_badge_creator(attendees)
-    attendees.collect do |name| 
+  def batch_badge_creator(speakers)
+    speakers.collect do |name| 
       badge_maker(name)
     end
   end
