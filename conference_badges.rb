0ba4e924acc4b985dac76c1d6f2f9e0afd7cd311
Jeffery Eckert <bromodo@gmail.com>
2019-10-14 00:56:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,7 @@ def badge_maker(name)
 end
   
   def batch_badge_creator(speakers)
-    speakers.collect {|name| badge_maker(name)}
+    speakers.collect do |name| 
+      badge_maker(name)
+    end
   end
